d94e134a4b15171f64ea18e4be05a54a88a29531
JinGoo <zzgg3040@naver.com>
2024-11-06 13:33:10 +0900

    feat: 구매 상품명과 수량을 객체로 파싱

---
+++ src/main/java/store/dto/Item.java
@@ -0,0 +1,34 @@
+package store.dto;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import store.exception.BusinessException;
+
+public class Item {
+
+    private static final String REGEX = "\\[([가-힣]+)-([0-9]+)\\]";
+
+    private final String name;
+    private final long quantity;
+
+    private Item(String name, long quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public static Item from(String itemInput) {
+        Matcher matcher = Pattern.compile(REGEX).matcher(itemInput);
+        if (matcher.find()) {
+            return new Item(matcher.group(1), Long.parseLong(matcher.group(2)));
+        }
+        throw new BusinessException("구매 정보가 유효하지 않습니다.");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+}
---
+++ src/test/java/store/dto/ItemTest.java
@@ -0,0 +1,21 @@
+package store.dto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ItemTest {
+
+    @DisplayName("[상품명-수량] 문자열로 입력되면 이름과 수량 정보를 추출하여 객체를 생성한다.")
+    @Test
+    void test1() {
+        // given
+        String itemInput = "[콜라-3]";
+        // when
+        Item item = Item.from(itemInput);
+        // then
+        assertEquals(item.getName(), "콜라");
+        assertEquals(item.getQuantity(), 3L);
+    }
+}
