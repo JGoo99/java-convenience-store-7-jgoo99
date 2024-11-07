977d40c44745d4cd5463821a23f91a683b9465fe
JinGoo <zzgg3040@naver.com>
2024-11-07 18:02:14 +0900

    feat: 구매 수량이 재고 수량을 초과하는 케이스

--- src/main/java/store/model/Item.java
+++ src/main/java/store/model/Item.java
@@ -23,17 +23,28 @@ public class Item {
             String productName = matcher.group(1);
             long quantity = Long.parseLong(matcher.group(2));
 
-            validateProductName(productName);
+            validate(productName, quantity);
             return new Item(productName, quantity);
         }
         throw new BusinessException("잘못된 입력입니다. 다시 입력해 주세요.");
     }
 
-    private static void validateProductName(String productName) {
+    private static void validate(String productName, long quantity) {
         Long productQuantity = ProductQuantityRepository.getInstance().findByName(productName);
         if (productQuantity == null) {
             throw new BusinessException("존재하지 않는 상품입니다. 다시 입력해 주세요.");
         }
+        if (productQuantity < quantity) {
+            throw new BusinessException("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public long getQuantity() {
+        return quantity;
     }
 
     @Override
--- src/test/java/store/model/ItemTest.java
+++ src/test/java/store/model/ItemTest.java
@@ -46,4 +46,15 @@ class ItemTest {
                 .isInstanceOf(BusinessException.class)
                 .hasMessageContaining("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
     }
+
+    @DisplayName("구매 수량이 재고 수량을 초과하는 경우 예외가 발생한다.")
+    @Test
+    void test4() {
+        // given
+        ProductQuantityRepository.getInstance().save("콜라", 10L);
+        // when & then
+        assertThatThrownBy(() -> Item.from("[콜라-11]"))
+                .isInstanceOf(BusinessException.class)
+                .hasMessageContaining("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+    }
 }
