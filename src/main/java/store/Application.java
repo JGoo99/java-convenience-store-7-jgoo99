e8ec727af4fac3435a5014c75b46f5f29b5f243c
JinGoo <zzgg3040@naver.com>
2024-11-11 01:21:39 +0900

    fix: 전역 처리 시 예외 타입 변경

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,14 +1,13 @@
 package store;
 
 import store.core.manager.ConvenienceRunner;
-import store.exception.BusinessException;
 
 public class Application {
     public static void main(String[] args) {
         try {
             ConvenienceRunner runner = ConvenienceRunner.stock();
             runner.open();
-        } catch (BusinessException e) {
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
